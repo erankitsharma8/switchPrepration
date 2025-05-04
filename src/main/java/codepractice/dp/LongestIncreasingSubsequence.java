1da5e68ab97ba49b2ea12dd4cb8e0fb825473e6f
Ankit Sharma <erankitsharma8@gmail.com>
2025-05-04 23:26:30 +0530

    DP problems added

--- src/main/java/codepractice/dp/LongestIncreasingSubsequence.java
+++ src/main/java/codepractice/dp/LongestIncreasingSubsequence.java
@@ -1,4 +1,28 @@
 package codepractice.dp;
 
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence {
+
+    private static int longestIncreasingSubsequence(int [] arr){
+           int n=arr.length;
+           int [] dp=new int[n];
+           Arrays.fill(dp,1);
+           for(int i=1;i<n;i++) {
+               int j = 0;
+               while (j < i) {
+                   if (arr[j] < arr[i] && dp[j] >= dp[i])
+                       dp[i] = dp[j] + 1;
+                   j++;
+               }
+           }
+               int max=1;
+               for(int num:dp)
+                   max=Math.max(max,num);
+               return max;
+    }
+    public static void main(String[] args) {
+              int [] arr=new int[]{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+              System.out.println("answer is: "+longestIncreasingSubsequence(arr));
+    }
 }
--- src/main/java/codepractice/dp/WordBreak.java
+++ src/main/java/codepractice/dp/WordBreak.java
@@ -1,4 +1,35 @@
 package codepractice.dp;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class WordBreak {
+
+    private static boolean wordBreak(String s, String[] dictionary) {
+        // code here
+        Set<String> dicSet = new HashSet<>(Arrays.asList(dictionary));
+        int n=s.length();
+        int maxLen=0;
+        for(String ss:dictionary)
+            maxLen=Math.max(maxLen,ss.length());
+
+        boolean [] dp=new boolean[n+1];
+        dp[0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=i-1;j>=Math.max(0,i-maxLen);j--){
+                if(dp[j] && dicSet.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        String s="ilike";
+        String [] str=new String[]{"i","like","gfg"};
+        System.out.println("answer is: "+ wordBreak(s,str));
+    }
 }
